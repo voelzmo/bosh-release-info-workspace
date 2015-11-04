8d0d89e4665e474fc91b8a8fcd78870424d9bdc0
Marco Voelz <marco.voelz@sap.com>
2015-11-04 22:15:20 +0100

    find licenses in packages

--- src/github.com/voelzmo/bosh-release-info/commands/file_list.go
+++ src/github.com/voelzmo/bosh-release-info/commands/file_list.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
+	"regexp"
 	"strings"
 
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
@@ -19,14 +21,23 @@ func FileListCommand(fs boshsys.FileSystem, comp boshcmd.Compressor, logger bosh
 	command := cli.Command{
 		Name:  "file-list",
 		Usage: "lists all files in all packages in this release",
+		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:  "sort-by-type",
+				Usage: "sort the files according to their filetype",
+			},
+		},
 		Action: func(c *cli.Context) {
 			if len(c.Args()) != 1 {
 				err := bosherr.Error("that's not how it works. provide the name to the release tarball!")
 				output.Fail(err, logger)
 			}
 			releasePath := c.Args()[0]
-			relasePathSplit := strings.Split(releasePath, "/")
-			releaseName := relasePathSplit[len(relasePathSplit)-1]
+			sortByType := c.Bool("sort-by-type")
+
+			// relasePathSplit := strings.Split(releasePath, "/")
+			// releaseName := relasePathSplit[len(relasePathSplit)-1]
+			releaseName := path.Base(releasePath)
 			fmt.Printf("Info for release: %s\n", releaseName)
 			fmt.Printf("\n")
 
@@ -51,10 +62,30 @@ func FileListCommand(fs boshsys.FileSystem, comp boshcmd.Compressor, logger bosh
 					output.Fail(bosherr.WrapError(err, "Failed reading package spec files:"), logger)
 				}
 
-				fmt.Printf("Files for package '%s': %s", pkg.Name, pkgSpecFiles)
+				if sortByType {
+					filesInPackage := strings.Split(pkgSpecFiles, "\n")
+					filesByType := make(map[string][]string)
+					for _, fileWithPath := range filesInPackage {
+						file := path.Base(fileWithPath)
+						re, _ := regexp.Compile(".*(LICENSE|license|License)[\\.md]??")
+						if re.MatchString(fileWithPath) {
+							fileSplit := strings.Split(file, ".")
+							fileType := fileSplit[len(fileSplit)-1]
+							filesByType[fileType] = append(filesByType[fileType], fmt.Sprintf("%s:%s", manifest.Name, fileWithPath))
+						} else {
 
-				fmt.Printf("\n")
+						}
 
+					}
+					for typeName, files := range filesByType {
+						fmt.Printf("Files for type '%s'\n: %s", typeName, strings.Join(files, "\n"))
+						fmt.Printf("\n")
+					}
+
+				} else {
+					fmt.Printf("Files for package '%s': %s", pkg.Name, pkgSpecFiles)
+					fmt.Printf("\n")
+				}
 			}
 		},
 	}
